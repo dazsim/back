e740d5891b12ef69b0932a72fb7c155d8401a42d
workshopcraft <darrensim42@gmail.com>
2019-08-22 10:28:15 +0100

    game now has controls and basic feedback

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,72 @@
-//This is our js for the game!
+// globals
+
+var ng = 'New Game'
+var se = "Settings"
+var bk = "Back"
+var sc = "Score: ";
+
+var fnt = "40px Tahoma";
+var speedo = "60px Tahoma";
+var gameTick = 0;
+
+var Key = {
+    _pressed: {},
+    W: 87,
+    A: 65,
+    D: 68,
+    S: 83,
+    isDown: function(keyCode) {
+        return this._pressed[keyCode];
+      },
+      
+    onKeydown: function(event) {
+    this._pressed[event.keyCode] = true;
+    },
+    
+    onKeyup: function(event) {
+    delete this._pressed[event.keyCode];
+      }
+}
+
+window.addEventListener('keyup', function(event) { Key.onKeyup(event); }, false);
+window.addEventListener('keydown', function(event) { Key.onKeydown(event); }, false);
+
+// global gamestate
+
+var position = 0;
+var car_x = 0;
+var car_y = 0;
+var car_speed = 0;
+var score = 0;
+var car_max_speed = 10;
+var current_road = 0;
+
+//This is our js for the game
+function checkBounds(e,x,y,z,r)
+{
+    px = e.pageX
+    py = e.pageY
+    
+    if ((px>x && px<z) && (py>y && py<r)) return true
+    return false
+}
+
+function getLineHeight(e)
+{
+    e.save()
+    
+    var lineHeight=e.measureText('M').width
+    e.restore()
+    return lineHeight
+}
+
+function resetGamestate()
+{
+    position = 0
+    car_x = 0
+    car_speed = 0
+    score = 0
+    car_x = a.width/2
+    car_max_speed = 100
+    
+}
