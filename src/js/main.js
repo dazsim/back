0fc47632458b740a96897905567d872d44b40bc9
workshopcraft <darrensim42@gmail.com>
2019-09-01 00:10:45 +0100

    speedo rendering now works

--- src/js/main.js
+++ src/js/main.js
@@ -55,6 +55,12 @@ var car_speed = 0;
 var score = 0;
 var car_max_speed = 120;
 var current_road = 0;
+var speed_colors = [
+    "#1f1",
+
+    "#fb0",
+    "#f00"
+];
 
 
 //This is our js for the game
@@ -86,4 +92,147 @@ function resetGamestate()
     car_max_speed = 120
     
 }
+
+function drawDigit(cx,x,y,value,size)
+{
+    
+    switch(value)
+    {
+    case 0:
+        drawTop(cx,x,y,size)
+        drawBottom(cx,x,y,size)
+        drawLeft(cx,x,y,size)
+        drawLeft(cx,x,y+size,size)
+        drawRight(cx,x,y,size)
+        drawRight(cx,x,y+size,size)
+        break
+    case 1:
+        drawRight(cx,x,y,size)
+        drawRight(cx,x,y+size,size)
+        break
+    case 2:
+        drawRight(cx,x,y,size)
+        drawLeft(cx,x,y+size,size)
+        drawTop(cx,x,y,size)
+        drawBottom(cx,x,y,size)
+        drawMiddle(cx,x,y,size)
+        break
+    case 3:
+        drawTop(cx,x,y,size)
+        drawBottom(cx,x,y,size)
+        drawMiddle(cx,x,y,size)
+        drawRight(cx,x,y,size)
+        drawRight(cx,x,y+size,size)
+        break
+    case 4:
+        drawMiddle(cx,x,y,size)
+        drawLeft(cx,x,y,size)
+        drawRight(cx,x,y,size)
+        drawRight(cx,x,y+size,size)
+        break
+    case 5:
+        drawRight(cx,x,y+size,size)
+        drawLeft(cx,x,y,size)
+        drawTop(cx,x,y,size)
+        drawBottom(cx,x,y,size)
+        drawMiddle(cx,x,y,size)
+        break
+    case 6:
+        drawTop(cx,x,y,size)
+        drawBottom(cx,x,y,size)
+        drawMiddle(cx,x,y,size)
+        drawLeft(cx,x,y,size)
+        drawLeft(cx,x,y+size,size)
+        drawRight(cx,x,y+size,size)
+        break
+    case 7:
+        drawTop(cx,x,y,size)
+        drawRight(cx,x,y,size)
+        drawRight(cx,x,y+size,size)
+        break
+    case 8:
+        drawTop(cx,x,y,size)
+        drawBottom(cx,x,y,size)
+        drawLeft(cx,x,y,size)
+        drawLeft(cx,x,y+size,size)
+        drawRight(cx,x,y,size)
+        drawRight(cx,x,y+size,size)
+        drawMiddle(cx,x,y,size)
+        break
+    case 9:
+        drawTop(cx,x,y,size)
+        drawBottom(cx,x,y,size)
+        drawLeft(cx,x,y,size)
+        drawRight(cx,x,y,size)
+        drawRight(cx,x,y+size,size)
+        drawMiddle(cx,x,y,size)
+        
+
+    }
+}
+
+function drawTop(cx,x,y,size)
+{
+    cx.beginPath()
+    cx.moveTo(x+size*0.1,y)
+    cx.lineTo(x+size*0.9,y)
+    cx.lineTo(x+size*0.8,y+size*0.1)
+    cx.lineTo(x+size*0.2,y+size*0.1)
+    cx.fill()
+}
+
+function drawBottom(cx,x,y,size)
+{
+    cx.beginPath()
+    cx.moveTo(x+size*0.1,y+2*size)
+    cx.lineTo(x+size*0.9,y+2*size)
+    cx.lineTo(x+size*0.8,y-size*0.1+2*size)
+    cx.lineTo(x+size*0.2,y-size*0.1+2*size)
+    cx.fill()
+}
+
+function drawMiddle(cx,x,y,size)
+{
+    cx.beginPath()
+    cx.moveTo(x+size*0.1,y+size)
+    cx.lineTo(x+size*0.2,y-size*0.1+size)
+    cx.lineTo(x+size*0.8,y-size*0.1+size)
+    cx.lineTo(x+size*0.9,y+size)
+    
+    cx.lineTo(x+size*0.8,y+size*0.1+size)
+    cx.lineTo(x+size*0.2,y+size*0.1+size)
+    
+    cx.fill()
+}
+function drawLeft(cx,x,y,size)
+{
+    cx.beginPath()
+    cx.moveTo(x,y+size*0.1)
+    cx.lineTo(x,y+size*0.9)
+    cx.lineTo(x+size*0.1,y+size*0.8)
+    cx.lineTo(x+size*0.1,y-size*0.1+size*0.2)
+    cx.lineTo(x,y+size*0.1)
+    cx.fill()
+}
+function drawRight(cx,x,y,size)
+{
+    cx.beginPath()
+    cx.moveTo(x+size,y+size*0.1)
+    cx.lineTo(x+size,y+size*0.9)
+    cx.lineTo(x+size-size*0.1,y+size*0.8)
+    cx.lineTo(x+size-size*0.1,y-size*0.1+size*0.2)
+    cx.fill()
+}
+
+function drawBar(cx,x,y,width,height,color)
+{
+    x.fillStyle = color;
+    cx.beginPath()
+    cx.moveTo(x,y)
+    cx.moveTo(x+width,y)
+    cx.moveTo(x+width,y+height)
+    cx.moveTo(x,y+height)
+    cx.fill()
+}
+
 
